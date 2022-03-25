e08f55a5dba245f3d948c2bcfc1d41e2c30df1bf
Lakeram Narine <nar19002@byui.edu>
2022-03-25 11:45:07 -0600

    added velocity

--- artillerySimulator/velocity.h
+++ artillerySimulator/velocity.h
@@ -1,138 +1,58 @@
-/*****************************************
-* HEADER FILE - VELOCITY
-*****************************************/
 #pragma once
-#include <iostream>
-#include <math.h>
-
-/*****************************************
-* VELOCITY ZERO
-* This is a Mock class made to ensure that
-* the unit tests will pass without using
-* VELOCITY itself.
-*****************************************/
-class VelocityZero
+#include "direction2.h"
+class Velocity
 {
-
 public:
+	// constructors for velosity, setting our 
+	//intital dx and dy
+	Velocity() : dx(0.0), dy(0.0) {}
+	Velocity(double dx, double dy) : dx(dx), dy(dy) {}
+	Velocity(double speed, Direction& direction) { setSpeedAngle(speed, direction); }
+	Velocity& operator =(const Velocity& rhs)
+	{
+		dx = rhs.dx;
+		dy = rhs.dy;
+		return *this;
+	}
 
-   // initalize the variables
-   double x = 0;
-   double y = 0;
-   double dx = 0;
-   double dy = 0;
-   double s = 0;
+	double getDX() { return dx; };
+	double getDY() { return dy; };
 
-   /*****************************************
-   * GET DX / DY
-   * These two methods will simply return
-   * the current value of dx / dy.
-   *****************************************/
-   double getDX(double dx) { return dx; }
-   double getDY(double dy) { return dy; }
+	void setDX(double dx) { this->dx = dx; };
+	void setDY(double dy) { this->dy = dy; };
+	void setSpeedAngle(double speed, Direction& direction);
+	//adds our new velocity to current velosity
+	void addDX(double dx) { setDX(getDX() + dx); };
+	void addDY(double dy) { setDY(getDY() + dy); };
+	void setDxDy(double dx, double dy) { this->dx = dx; this->dy = dy; }
 
-   /*****************************************
-   * SET DX / DY
-   * These two methods will set dx / dy to
-   * a new value.
-   *****************************************/
-   double setDX(double dx) { return dx = 2.0; }
-   double setDY(double dy) { return dy = 1.0; }
+	void addV(const Velocity& velocity)
+	{
+		dx += velocity.dx;
+		dy += velocity.dy;
+	}
+	Velocity& operator += (const Velocity& rhs)
+	{
+		addV(rhs);
+		return *this;
+	}
+	void reverse()
+	{
+		dx *= -1.0;
+		dy *= -1.0;
+	}
+	// calculte the Speed of the Lander, this will be need in the Display in the Game class.
+	double getSpeed() { return sqrtl(this->dx * this->dx + this->dy * this->dy); };
 
-   /*****************************************
-   * ADD DX / DY
-   * These two methods will add the current
-   * x / y position to the new dx / dy.
-   *****************************************/
-   double addDX(double x, double dx)
-   {
-      x + dx;
-      return dx = 3.0;
-   }
-   double addDY(double y, double dy)
-   {
-      y + dy;
-      return dy = 3.0;
-   }
 
-   /*****************************************
-   * GET SPEED
-   * This method will use the Pythagoreum
-   * Theorum to find the speed:
-   *    a^2 + b^2 = c^2
-   * We need to find the equivalent of c^2,
-   * so we will reorder to fit our needs:
-   *    c = sqrt(a^2 + b^2)
-   * In our variables:
-   *    s = sqrt(dx^2 + dy^2)
-   *****************************************/
-   double getSpeed(double s, double dx, double dy)
-   {
-      return s = sqrt(dx * dx + dy * dy);
-   }
+private:
+	double dx;
+	double dy;
 };
 
-/*****************************************
-* VELOCITY
-*****************************************/
-class Velocity : VelocityZero
+void Velocity::setSpeedAngle(double speed, Direction& direction)
 {
-protected:
+	setDX(speed * direction.getDx());
+	setDY(speed * direction.getDy());
 
-   // initialize the variables
-   double x = 0;
-   double y = 0;
-   double dx = 0;
-   double dy = 0;
-   double s = 0;
-
-public:
-
-   /*****************************************
-   * GET DX / DY
-   * These two methods will simply return
-   * the current value of dx / dy.
-   *****************************************/
-   virtual double getDX(double dx) { return dx; }
-   virtual double getDY(double dy) { return dy; }
-
-   /*****************************************
-   * SET DX / DY
-   * These two methods will set dx / dy to
-   * a new value.
-   *****************************************/
-   virtual double setDX() { return dx = 2.0; }
-   virtual double setDY() { return dy = 1.0; }
-
-   /*****************************************
-   * ADD DX / DY
-   * These two methods will add the current
-   * x / y position to the new dx / dy.
-   *****************************************/
-   virtual double addDX(double x, double dx)
-   {
-      x + dx;
-      return dx = 3.0;
-   }
-   virtual double addDY(double y, double dy)
-   {
-      y + dy;
-      return dy = 3.0;
-   }
-
-   /*****************************************
-   * GET SPEED
-   * This method will use the Pythagoreum
-   * Theorum to find the speed:
-   *    a^2 + b^2 = c^2
-   * We need to find the equivalent of c^2,
-   * so we will reorder to fit our needs:
-   *    c = sqrt(a^2 + b^2)
-   * In our variables:
-   *    s = sqrt(dx^2 + dy^2)
-   *****************************************/
-   virtual double getSpeed(double s, double dx, double dy)
-   {
-      return s = sqrt(dx * dx + dy * dy);
-   }
-};
+}
